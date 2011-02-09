ca735a7b6ea2b3737918186b1b9731a8a16b9806
Tadashi Ogino <ogino@leafnet.jp>
2011-02-09 22:00:42 +0900

    Add OAuth, Fix Time Line View.

---
+++ Classes/src/OAuth/OAuthCore.h
@@ -0,0 +1,12 @@
+//
+//  OAuthCore.h
+//
+//  Created by Loren Brichter on 6/9/10.
+//	Modified by Tadashi Ogino on 9/2/11.
+//  Copyright 2010 Loren Brichter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *OAuthorizationHeader(NSURL *url, NSString *method, NSData *body, NSString *_oAuthConsumerKey, NSString *_oAuthConsumerSecret,
+									  NSString *_oAuthToken, NSString *_oAuthTokenSecret, BOOL isRequest);
